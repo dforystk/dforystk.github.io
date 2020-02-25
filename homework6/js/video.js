c7e3c2669fbb84d2e77cedf113f69a6038d29793
dforystk <dforystk@umich.edu>
2020-02-25 18:06:50 -0500

    Update video.js

--- homework6/js/video.js
+++ homework6/js/video.js
@@ -1,52 +1,70 @@
 var video;
+var volume;
 
 function getVid(){
 	video = document.querySelector("#myVideo"); 
 }
 
 function playVid() { 
-
+	video.play()
 	console.log("Play Video");
+	console.log(video)
+	volume = document.querySelector("#volume");
+	console.log(volume)
+	volume.innerHTML = video.volume * 100 + "%"
 } 
 
 function pauseVid() { 
-
+	video.pause()
 	console.log("Pause Video");
 } 
 
 function decreaseSpeed() { 
-
-  	console.log("Speed is "+ );
+	video.playbackRate -= .1
+  	console.log("Speed is "+ video.playbackRate);
 } 
 
 function increaseSpeed() {
-
-	console.log("Speed is "+ );
+	video.playbackRate += .1
+	console.log("Speed is "+ video.playbackRate);
 } 
 
-function skipAhead() {
+// function skipAhead() {
 
-	console.log("Current location is "+ );
-} 
+// 	console.log("Current location is "+ );
+// } 
 
 function mute() { 
-  	
-  		console.log("Unmuted");
-  	
+	let mute = document.querySelector("#mute")
+	console.log(video.muted)
+  	if (video.muted){
+  		video.muted=false;
+		console.log("Unmuted");
+  	}
+    else{
+    	video.muted = true;
       	console.log("Muted");
+	}
+	console.log(mute)
+	var button= document.querySelector('#mute')
+	button.innerHTML="Unmuted"
 }
 
 function changeVolume() {
-;	console.log("Volume is ");
+	let slider = document.querySelector("#volumeSlider")
+	console.log(slider.value)
+	video.volume = slider.value/100
+	console.log("Volume is " + video.volume)
+	volume.innerHTML = slider.value + "%"
 }
        
 
-function gray() { 
+// function gray() { 
 
-	console.log("In grayscale")
-}
+// 	console.log("In grayscale")
+// }
 
-function color() {
+// function color() {
 
-	console.log("In color") 
-}
+// 	console.log("In color") 
+// }
