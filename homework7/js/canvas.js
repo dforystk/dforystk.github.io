03f36ec2fdb5c99eca8aa291387d8c3cc2d7fd84
dforystk <55510098+dforystk@users.noreply.github.com>
2020-03-26 11:14:30 -0400

    Add files via upload

---
+++ homework7/js/canvas.js
@@ -0,0 +1,46 @@
+// Variables!
+var color ="red";
+var radius = 15;
+var x = 50;
+var y = 150;
+var canvas = document.querySelector('#canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.fillstyle = 'red'
+
+//You will want to add more
+
+
+//Listeners!!
+//Add a listener for loading the window
+window.addEventListener('onload', function(e){
+	console.log("page loaded");
+
+})
+//Add a listener for the mouse movement
+
+canvas.addEventListener('mousemove', function(e){
+	draw();
+	console.log(this);
+	console.log(e);
+})
+
+//Add a listener for the touch move
+canvas.addEventListener('mousedown', function(e){
+	console.log(e);
+})
+
+//Add a listener for the keydown
+window.addEventListener('keydown', function(e){
+	console.log(this);
+	console.log(e);
+})
+
+// Functions!
+// I would add a function for draw
+function draw (x,y) {
+        //use x and y below
+        console.log("I'M GONNA DRAW")
+        //this always draws in position 50,50
+        ctx.fill();
+    }
