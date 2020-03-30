6026199dce3afb620cb2ffeef1d10b64d2218eb2
dforystk <dforystk@umich.edu>
2020-03-30 16:47:31 -0400

    change color and clear canvas working

--- homework7/js/canvas.js
+++ homework7/js/canvas.js
@@ -15,55 +15,70 @@ function getMousePos(canvas, evt) {
     };
 }
 
+function changeColor(e){
+	if (e.keyCode == 66){
+		color = "blue"
+	};
+	if (e.keyCode == 82){
+		color = "red"
+	};
+	if (e.keyCode == 71){
+		color = "green"
+	};
+	if (e.keyCode == 89){
+		color = "yellow"
+	};
+}
 
+function clearCanvas(e){
+	if (e.keyCode == 32){
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+	}
+}
 
 //Listeners!!
 //Add a listener for loading the window
+
 window.addEventListener('load', function(e){
-	console.log('Hello');
+	console.log('Ready to draw?');
 	canvas.height = window.innerHeight * .75;
 	canvas.width = window.innerWidth * .75;
 })
+
 //Add a listener for the mouse movement
 
 canvas.addEventListener("mousemove", function (e) {
     var mousePos = getMousePos(canvas, e);
     console.log(mousePos.x + ',' + mousePos.y);
-    draw(mousePos.x, mousePos.y)
+    draw(mousePos.x, mousePos.y);
 }, false);
 
 
 //Add a listener for the touch move
-canvas.addEventListener('mousedown', function(e){
-	console.log(this);
-	console.log(e);
-})
+
+// canvas.addEventListener('mousedown', function(e){
+// 	console.log(this);
+// 	console.log(e);
+// })
 
 //Add a listener for the keydown
-canvas.addEventListener('keydown', function(e){
+
+window.addEventListener("keydown", function(e){
 	console.log(this);
-	console.log(e);
+	changeColor(e);
+	clearCanvas(e);
 })
 
 // Functions!
 // I would add a function for draw
 function draw (x,y) {
         //use x and y below
-        console.log("I'M GONNA DRAW")
+        console.log("Drawing...")
+        ctx.beginPath();
         ctx.fillStyle = color;
         ctx.arc(x,y,15,0,2 * Math.PI);
         ctx.fill();
+        
     }
 
-// function drawCircle (x,y) {
-//         //use x and y below
-//         console.log("Drawing Circle")
-//         ctx.fill();
-//         ctx.fillStyle = color;
-//         ctx.arc(x,y,15,0,2 * Math.PI);
-//     }
-
-// --------------MOUSE POSITION--------------
-
-
-
+f
