50a2a2ae22e3aac5fd080892abf0eb6d4071bae6
dforystk <dforystk@umich.edu>
2020-03-30 15:35:14 -0400

    Canvas resizes

--- homework7/js/canvas.js
+++ homework7/js/canvas.js
@@ -6,31 +6,34 @@ var y = 150;
 var canvas = document.querySelector('#canvas');
 var ctx = canvas.getContext('2d');
 
-ctx.fillstyle = 'blue'
+ctx.fillStyle = color
 
 //You will want to add more
 
 
 //Listeners!!
 //Add a listener for loading the window
-window.addEventListener('onload', function(e){
-	canvas.width = window.innerwidth * .75
+window.addEventListener('load', function(e){
+	console.log('Hello');
+	canvas.height = window.innerHeight * .75;
+	canvas.width = window.innerWidth * .75;
 })
 //Add a listener for the mouse movement
 
 canvas.addEventListener('mousemove', function(e){
-	draw();
+	draw(25,25);
 	console.log(this);
 	console.log(e);
 })
 
 //Add a listener for the touch move
 canvas.addEventListener('mousedown', function(e){
+	console.log(this);
 	console.log(e);
 })
 
 //Add a listener for the keydown
-window.addEventListener('keydown', function(e){
+canvas.addEventListener('keydown', function(e){
 	console.log(this);
 	console.log(e);
 })
@@ -42,4 +45,5 @@ function draw (x,y) {
         console.log("I'M GONNA DRAW")
         //this always draws in position 50,50
         ctx.fill();
+        ctx.arc(15,15,15,0,2 * Math.PI);
     }
