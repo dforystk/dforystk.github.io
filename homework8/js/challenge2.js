1d3bed6ad64be00126883f72970e313d8538331b
dforystk <dforystk@umich.edu>
2020-04-17 12:37:48 -0400

    Challenge 2 done

--- homework8/js/challenge2.js
+++ homework8/js/challenge2.js
@@ -1,10 +1,23 @@
-sameAddress.onchange = function(){
-	console.log("sup")
-	let x = document.querySelector('#bill')
-	let y = document.querySelector('#home')
-	if (this.checked){
-		console.log(x.value)
-		y.value = x.value
-		y.disabled = true
-	}
+// sameAddress.onchange = function(){
+// 	console.log("hi")
+// 	let x = document.querySelector('#bill')
+// 	let y = document.querySelector('#home')
+// 	if (this.checked){
+// 		console.log(x.value)
+// 		y.value = x.value
+// 		y.disabled = true
+// 	}
+// }
+
+function setHomeAddress(){
+  if ($("#sameAddress").is(":checked")) {
+    $('#home').val($('#bill').val());
+    $('#home').attr('disabled', 'disabled');
+  } else {
+    $('#home').removeAttr('disabled');
+  }
 }
+
+$('#sameAddress').click(function(){
+  setHomeAddress();
+})
