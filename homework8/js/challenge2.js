143ff69f1e523e667c5a3b8a0d6ede833fafed2e
dforystk <dforystk@umich.edu>
2020-04-17 12:49:42 -0400

    challenge 2 background

--- homework8/js/challenge2.js
+++ homework8/js/challenge2.js
@@ -13,8 +13,11 @@ function setHomeAddress(){
   if ($("#sameAddress").is(":checked")) {
     $('#home').val($('#bill').val());
     $('#home').attr('disabled', 'disabled');
+    $('#home').css('background-color', '#d1d1d1')
   } else {
     $('#home').removeAttr('disabled');
+    $('#home').css('background-color','#FFFFFF')
+    $('#home').val('');
   }
 }
 
