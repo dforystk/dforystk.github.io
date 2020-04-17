ee92ed7c3e60b95bd76063bf59a829d52352752c
dforystk <dforystk@umich.edu>
2020-04-17 18:24:27 -0400

    Challenge 5 working

--- homework8/js/jschallenge5.js
+++ homework8/js/jschallenge5.js
@@ -1,6 +1,12 @@
 $(document).ready(function (){
 	$('.thumbnail').hover(function (e) {
+		$('#display').css('background-image', "url(" + $(this).attr('src') + ")");
 		$('#display').html($(this).attr('alt'));
-	});
+	$('.thumbnail').mouseleave(function(e){
+		$('#display').css('background-image','')
+		$('#display').html('Hover over an image below to display the image and the alt text.')
+	})
 
+
+	});
 });
