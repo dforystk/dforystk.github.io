ff5d73777e08e50cd0bedfdce2e1fa9c36f1d322
dforystk <dforystk@umich.edu>
2020-04-17 12:19:41 -0400

    Challenge 1 done

--- homework8/js/challenge1.js
+++ homework8/js/challenge1.js
@@ -1,11 +1,18 @@
-subscribe.addEventListener('change', function() {
-	console.log("here")
-	var x = document.querySelector("#emailDiv");
-	if (this.checked){
-		x.style.display="block";
-	}
-	else{
-		x.style.display = "none";
-	}
-})
+// subscribe.addEventListener('change', function() {
+// 	console.log("here")
+// 	var x = document.querySelector("#emailDiv");
+// 	if (this.checked){
+// 		x.style.display="block";
+// 	}
+// 	else{
+// 		x.style.display = "none";
+// 	}
+// })
 
+$("#subscribe").click(function () {
+    if ($(this).is(":checked")) {
+        $("#emailDiv").show();
+    } else {
+        $("#emailDiv").hide();
+    }
+});
